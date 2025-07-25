7055a917948b7beb9a285c35958cb5dbad9d167b
LukaSilagadze <lukasilagadze55@gmail.com>
2025-07-25 22:38:57 +0400

    add

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer>
+            <h1>footer</h1>
+            <p>&copy; {new Date().getFullYear()} website footer</p>
+        </footer>
+    );
+}
+
+export default Footer
